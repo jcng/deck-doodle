4b0cda507c1eaa251b67b830901a3314166ba66d
jcng <joshuang.contact@gmail.com>
2018-12-14 17:08:19 -0800

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import DeckDoodle from './components/DeckDoodle';
+import './css/main.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Placeholder test data
+const CARDS = [
+  { id: 1, name: 'Lightning Bolt', image: 'https://via.placeholder.com/100x150' },
+  { id: 2, name: 'Snapcaster Mage', image: 'https://via.placeholder.com/100x150' },
+  { id: 3, name: 'Thoughtseize', image: 'https://via.placeholder.com/100x150' },
+  { id: 4, name: "Kolaghan's Command", image: 'https://via.placeholder.com/100x150' },
+  { id: 5, name: 'Opt', image: 'https://via.placeholder.com/100x150' },
+  { id: 6, name: 'Inquisition of Kozilek', image: 'https://via.placeholder.com/100x150' },
+  { id: 7, name: 'Crackling Drake', image: 'https://via.placeholder.com/100x150' },
+  { id: 8, name: 'Liliana of the Veil', image: 'https://via.placeholder.com/100x150' },
+  { id: 9, name: 'Fatal Push', image: 'https://via.placeholder.com/100x150' },
+  { id: 10, name: 'Dreadbore', image: 'https://via.placeholder.com/100x150' }
+]
+
+ReactDOM.render(<DeckDoodle cards={CARDS}/>, document.getElementById('root'));
